329cc91b83b81e9a1b68de583e204e6d9ccb9593
MendFeld <53190630+MendFeld@users.noreply.github.com>
2019-07-25 04:13:09 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FormLoad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFormLoad = new System.Windows.Forms.Label();
+            this.lblButtonClick = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(28, 150);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(75, 23);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click me !";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.BtnClickMe_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(109, 150);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(190, 150);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lblFormLoad
+            // 
+            this.lblFormLoad.AutoSize = true;
+            this.lblFormLoad.Location = new System.Drawing.Point(86, 72);
+            this.lblFormLoad.Name = "lblFormLoad";
+            this.lblFormLoad.Size = new System.Drawing.Size(35, 13);
+            this.lblFormLoad.TabIndex = 3;
+            this.lblFormLoad.Text = "label1";
+            // 
+            // lblButtonClick
+            // 
+            this.lblButtonClick.AutoSize = true;
+            this.lblButtonClick.Location = new System.Drawing.Point(86, 98);
+            this.lblButtonClick.Name = "lblButtonClick";
+            this.lblButtonClick.Size = new System.Drawing.Size(35, 13);
+            this.lblButtonClick.TabIndex = 4;
+            this.lblButtonClick.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 287);
+            this.Controls.Add(this.lblButtonClick);
+            this.Controls.Add(this.lblFormLoad);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFormLoad;
+        private System.Windows.Forms.Label lblButtonClick;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormLoad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFormLoad.Text = "This text is set on startup !";
+        }
+
+        private void BtnClickMe_Click(object sender, EventArgs e)
+        {
+            lblButtonClick.Text = "This text is set on button click !";
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            lblButtonClick.Text = string.Empty;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
